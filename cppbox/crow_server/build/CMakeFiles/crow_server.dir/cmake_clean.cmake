554cff6a701495c8dd59aeb5396ef3a942e54b42
BLuglio <biagio.luglio@gmail.com>
2020-04-18 16:34:22 +0200

    initial commit

---
+++ cppbox/crow_server/build/CMakeFiles/crow_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crow_server.dir/main.o"
+  "crow_server.pdb"
+  "crow_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crow_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
